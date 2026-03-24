6ae8e7f8e2e87839a93e5ea51ab4a5ea616144e1
Marius Morf <mmo159388@stud.gibb.ch>
2026-03-24 10:45:33 +0100

    Website nearly finished

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var modal = document.getElementById("myModal");
+
+    var modalLink = document.getElementById("myBtn");
+
+    var span = document.getElementsByClassName("close")[0];
+
+    modalLink.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+})
